4e1f2823b1c0750b95710f36ff73d8b465361565
Your Name <you@example.com>
2025-03-26 12:01:52 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Learn from './components/Hello';
+import Learning from './components/Learning'; 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Learn />
+        <Learning/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Learning.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { render } from '@testing-library/react';
+
+class Learning extends Component{
+    render(){
+        return <h1>Hello Render</h1>
+    }
+}
+
+export default Learning; 
