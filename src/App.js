828bc3423d82046dbbe663f1c439fd6be574807a
Your Name <you@example.com>
2025-04-04 23:08:53 +0600

    App.js user login section Updated

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import React from 'react';
  class App extends React.Component {
 
   state = {
-    username: ""
+    email: "",
+    password: "",
+   
   }
-  Changedata = (e) => {
-      let val = e.target.name; //username
-      let myvalue = e.target.value; //GettingValue
-      this.setState({[val]: myvalue}); //value set on the state
+  formSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.email, this.state.password);
+   
   }
+ 
 
   render() {
-
     return (
       <div className='App'>
-        <h1>Hello {this.state.username}</h1>
+        <h1>{this.state.email}</h1>
+        <h1>{this.state.password}</h1>
 
         <div>
-          <form>
+          <form onSubmit={this.formSubmit}>
             <div class="form-group" style={{marginLeft:200}}>
                 <label>User Name:</label> <br></br>
-                <input type='text' name='username' onChange={this.Changedata}></input><br></br>
+                <input type='email' name='email' onChange={(event)=>{this.setState({email:event.target.value})}}></input><br></br>
+                <input type='password' name='password'onChange={(event)=>{this.setState({password:event.target.value})}}></input><br></br>
+                
                 <button type='submit'>Submit</button>
             </div>
           </form>
