07500cbc60d667804ca194f26b757ea73c223bf1
Your Name <you@example.com>
2025-04-05 09:59:33 +0600

    Login Panel Email and Password Required section Updated

--- src/App.js
+++ src/App.js
@@ -4,11 +4,29 @@ import React from 'react';
   state = {
     email: "",
     password: "",
+    emailError: "",
+    passwordError: "",
    
+  }
+
+  validate(){
+    if(!this.state.email>0 && !this.state.password>0){
+      this.setState({emailError: "Email and Password required"})
+
+    }else if(!this.state.email>0){
+      this.setState({emailError: "Email Required"})
+    }else if(!this.state.password>0){
+      this.setState({passwordError: "Password Required"})
+    }else{
+      return true; 
+    }
   }
   formSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state.email, this.state.password);
+    if(this.validate()){
+      alert("Form Submit Done");
+    }
+    // console.log(this.state.email, this.state.password, this.state.emailError, this.state.passwordError);
    
   }
  
@@ -16,16 +34,21 @@ import React from 'react';
   render() {
     return (
       <div className='App'>
-        <h1>{this.state.email}</h1>
+        <h1>Login Panel</h1>
         <h1>{this.state.password}</h1>
 
         <div>
           <form onSubmit={this.formSubmit}>
             <div class="form-group" style={{marginLeft:200}}>
-                <label>User Name:</label> <br></br>
+                <label>Email:</label> <br></br>
+
                 <input type='email' name='email' onChange={(event)=>{this.setState({email:event.target.value})}}></input><br></br>
+                <strong style={{color: "red"}}>{this.state.emailError}</strong><br></br>
+
+                <label>Password:</label> <br></br>
                 <input type='password' name='password'onChange={(event)=>{this.setState({password:event.target.value})}}></input><br></br>
-                
+                <strong style={{color: "red"}}>{this.state.passwordError}</strong><br></br>
+
                 <button type='submit'>Submit</button>
             </div>
           </form>
